073682df0bafe3f944f250577eebe79ee56d0bae
general <general@situation.(none)>
2013-04-11 06:36:44 +0400

    compojure

--- project-tofu/project.clj
+++ project-tofu/project.clj
@@ -1,6 +1,7 @@
 (defproject project-tofu "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :description "TOFU!"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                                  [compojure "1.1.5"]
+                                  [hiccup "1.0.3"]]
+  :plugins [[lein-ring "0.8.3"]]
+  :ring {:handler project-tofu.core/app})
--- project-tofu/src/project_tofu/core.clj
+++ project-tofu/src/project_tofu/core.clj
@@ -1,6 +1,24 @@
-(ns project-tofu.core)
+(ns project-tofu.core 
+  (:use compojure.core
+    [hiccup.middleware :only (wrap-base-url)]
+    [hiccup core page])
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            [compojure.response :as response]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn version [] 
+  (print "0.0.2"))
+
+(defn index-page []
+  (html5
+    [:head [:title "Hello World"]] 
+    [:body [:h1 "Hello World"]]))
+
+(defroutes main-routes
+    (GET "/" [] (index-page))
+    (route/resources "/")
+    (route/not-found "Page not found"))
+
+(def app
+    (-> (handler/site main-routes)
+              (wrap-base-url)))
