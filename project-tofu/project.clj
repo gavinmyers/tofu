ed20fc1a0b8395dce5ffcf1160ac00bdd82e2f40
gavin <gavin@gavinm.com>
2013-05-19 20:46:35 -0500

    Update project.clj

--- project-tofu/project.clj
+++ project-tofu/project.clj
@@ -1,7 +1,12 @@
 (defproject project-tofu "0.1.0-SNAPSHOT"
   :description "TOFU!"
+  :min-lein-version "2.0.0"
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                                  [compojure "1.1.5"]
-                                  [hiccup "1.0.3"]]
-  :plugins [[lein-ring "0.8.3"]]
-  :ring {:handler project-tofu.core/app})
+                 [ring "1.1.8"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [compojure "1.1.5"]
+                 [de.ubercode.clostache/clostache "1.3.1"]]
+  :main tofu.core)
